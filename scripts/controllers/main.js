bd156da2599e20f5ffc181e0c066d2109943f6b5
sivasath <sivasakthi.bala15@gmail.com>
2017-08-27 23:45:57 -0400

    UI changes for sorting completed

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,24 +1,25 @@
-angular.module("todoListApp")
+'use strict';
 
-.controller('mainCtrl', function($scope,dataService) {  //dataservice is included in the para to give access to the particular service
-  $scope.addTodo = function() {    
-    var todo={name: "This is a new todo!"}
-    $scope.todos.push(todo);
+angular.module('todoListApp')
+.controller('mainCtrl', function($scope, dataService) {
+  $scope.addTodo = function() {
+    var todo = {name: "This is a new todo."};
+    $scope.todos.unshift(todo);
   };
   
-  dataService.gettodos(function(response){  //callback is necessary because of the asynchronous nature of javaScript, response contains the data returned by the url
-    
-    console.log(response.data);
-    $scope.todos =  response.data;
-    
-    $scope.deleteTodo = function(todo, $index){
-      dataService.deleteTodo(todo, $index);
-      $scope.todos.splice($index,1);
-    };
-    
-    $scope.saveTodo = function(todo, $index){
-      dataService.saveTodo(todo, $index);
-    };
+  $scope.helloWorld = dataService.helloWorld;
   
-  });
-});
+  dataService.getTodos(function(response) { 
+      console.log(response.data);  
+      $scope.todos = response.data;
+    });
+  
+  $scope.deleteTodo = function(todo, $index) {
+    dataService.deleteTodo(todo);
+    $scope.todos.splice($index, 1);
+  };
+  
+  $scope.saveTodo = function(todo) {
+    dataService.saveTodo(todo);
+  };
+})
